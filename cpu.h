cbe17c3340b6846ddad263868ba443030c20033c
shinyquagsire23 <mtinc2@gmail.com>
2015-09-20 19:55:00 -0700

    Bump up ROM translation cache to 4MB

--- cpu.h
+++ cpu.h
@@ -136,7 +136,7 @@ s32 translate_block_arm(u32 pc, translation_region_type translation_region,
 s32 translate_block_thumb(u32 pc, translation_region_type translation_region,
  u32 smc_enable);
 
-#define ROM_TRANSLATION_CACHE_SIZE (1024 * 512 * 4)
+#define ROM_TRANSLATION_CACHE_SIZE (1024 * 512 * 8)
 #define RAM_TRANSLATION_CACHE_SIZE (1024 * 384 * 2)
 #define BIOS_TRANSLATION_CACHE_SIZE (1024 * 128 * 2)
 #define TRANSLATION_CACHE_LIMIT_THRESHOLD (1024 * 32)
